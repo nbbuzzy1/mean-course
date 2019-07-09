6e49efd09ef96451006f5ea81e9af2d1890ff995
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-07-09 06:58:23 -0400

    added buttons to post-list component to edit and delete posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -17,7 +17,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = {title, content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
