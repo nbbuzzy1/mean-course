2feaf1baedaf340744e9f13e2258a4e837d3b2e3
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-07-18 06:42:25 -0400

    added mongodb and mongoose functionality for getting posts and deleting posts

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const url = process.env.MONGODB_URL;
 
-mongoose.connect(url).then(() => {
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
   console.log('connected to database')
 }).catch(() => {
   console.log('connection failed')
@@ -43,24 +43,23 @@ app.post('/api/posts', (req, res, next) => {
   })
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'asdfasdf',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'as2343dfasdf',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!!!'
-    },
-  ]
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-  })
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  });
 });
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({
+      message: 'Post deleted'
+    })
+  })
+})
 
 module.exports = app;
 
