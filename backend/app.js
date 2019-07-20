28e4b3b26725270caa88cfcb2a2f854b39ac0957
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-07-20 08:18:36 -0400

    finished edit post feature

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   next();
 });
@@ -44,6 +44,18 @@ app.post('/api/posts', (req, res, next) => {
     })
   });
 });
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+  Post.updateOne({_id: req.params.id}, post).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: 'Update successful' })
+  })
+})
 
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -13,7 +13,7 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
-  private post: Post;
+  post: Post;
 
   constructor(public postsService: PostsService,
               public route: ActivatedRoute) {}
@@ -31,11 +31,17 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+
     form.resetForm();
   }
 }
