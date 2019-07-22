bbaae57cfcd2bf88d7baa4c24114a36c2ca50ade
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-07-22 06:40:10 -0400

    added button to post-create to pick a file

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -27,7 +27,7 @@ export class PostCreateComponent implements OnInit {
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content }
+          this.post = {id: postData._id, title: postData.title, content: postData.content };
         })
       } else {
         this.mode = 'create';
